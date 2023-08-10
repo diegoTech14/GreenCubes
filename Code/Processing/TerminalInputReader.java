7cd293aee317744ad195e855d82292afc8d4e5ca
Marco Rodriguez <marco031120@gmail.com>
2023-08-09 18:47:31 -0600

    processing

--- Code/Processing/TerminalInputReader.java
+++ Code/Processing/TerminalInputReader.java
@@ -1,53 +1,50 @@
-// Se comunica con el puerto serial para leer datos enviados desde Arduino
 import processing.serial.*;
 
-public class TerminalInputReader {
-    // Objeto Serial se comunica con arduino
-    private Serial arduino;
+Serial puerto;
+int distancia1, distancia2, distancia3;
 
-    /* Variable para almacenar datos del puerto serial que se esta usando, en mi caso COM1.
-     * Para saber el puerto con arduino conectado:
-     * 1. Dentro del IDE de Arduino, ir a Herramientas
-     * 2. Opcion de Puerto
-     * 3. El que este seleccionado es el que hay que poner aqui
-     */
-    private String serialData = "";
+void setup() {
+  size(400, 300);
+  puerto = new Serial(this, "COM3", 9600);
+}
 
-    public TerminalInputReader(String portName, int baudRate) {
-        arduino = new Serial(null, portName, baudRate);
-    }
+void draw() {
+  terminal();
+  
+  // Check if cubo is found or not based on the distances
+  if (distancia1 < 50) {
+    println("Cubo encontrado en el sensor 1");
+  } else {
+    println("Cubo no encontrado en el sensor 1");
+  }
+}
 
-    // Habria que hacer cambios dependiendo de la forma en el que se manejen finalmente los prints ya que cada salto de linea
-    // deja de recibir informacion. Eso me hace pensar que sera necesario hacer ajustes en arduino o aca pero actualmente no hay certeza.
-    public void readData() {
-        // Revisa si hay datos disponibles para leer
-        while (arduino.available() > 0) {
-            // Lee un carácter del puerto serial
-            char savedChar = (char) arduino.read(); 
-            // Si el carácter es un salto de línea, muestra el mensaje recibido
-            if (savedChar == '\n') {
-                System.out.println("Mensaje recibido: " + serialData);
-                // Reinicia la variable para el próximo mensaje
-                serialData = "";
-            } else {
-                // Almacena el carácter en la variable serialData
-                serialData += savedChar; 
-            }
+void terminal() {
+  while (puerto.available() > 0) {
+    String datos = puerto.readStringUntil('\n');
+    if (datos != null) {
+      datos = datos.trim();
+      println("Esto recibí: " + datos);
+
+      if (datos.startsWith("Distance ")) {
+        int posInicioValor = datos.indexOf(":") + 2; // Posición justo después del ": "
+        int valor = Integer.parseInt(datos.substring(posInicioValor));
+
+        int sensor = int(datos.charAt(9) - '0');
+
+        if (sensor == 1) {
+          distancia1 = valor;
+        } else if (sensor == 2) {
+          distancia2 = valor;
+        } else if (sensor == 3) {
+          distancia3 = valor;
         }
+      }
     }
-
-    public static void main(String[] args) {
-        // Aca hay que hacer ajustes si es necesario
-        String portName = "COM1";
-        // Comunicacion serial tiene que estar igual en Arduino
-        int baudRate = 9600;
-
-        TerminalInputReader inputReader = new TerminalInputReader(portName, baudRate);
-
-        while (true) {
-            inputReader.readData();
-        }
-    }
+  }
+  //println("Distance 1: " + distancia1 + " cm");
+  //println("Distance 2: " + distancia2 + " cm");
+  //println("Distance 3: " + distancia3 + " cm");
 }
 
 
